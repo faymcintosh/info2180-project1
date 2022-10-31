d77cc0145dd7a48070faa656c2ea2222e90eb9f2
Fay Mcintosh <fay.mcintosh17@gmail.com>
2022-10-30 19:10:10 -0500

    Updated and Added initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
 /* Add your JavaScript to this file */
+
+window.onload = function () {
+
+    const button = document.querySelectorAll('.btn');
+    
+   button[1].addEventListener('click', function(event) {
+       event.preventDefault();
+       var  container = document.getElementsByClassName("message");
+      
+       var email= document.getElementById("email").value;
+       var para = document.createElement("p") ;
+       
+       if(email!="")
+       {
+             msg = document.createTextNode('Thank You ' + '' (email) + ('your email has been send our mail'));
+  
+        }else
+        {
+         msg = document.createTextNode('Please enter a valid email address');
+        }
+        if (container.innerHTML=""){
+            container.removeChild(container.lastElementChild);  
+        }
+
+     
+        para.appendChild(msg)
+       container[0].appendChild(para);
+  })
+
+
+  }
+
+ 
